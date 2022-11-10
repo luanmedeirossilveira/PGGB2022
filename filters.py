bbe67372de015a38df64f5338eb96fac01515584
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2022-11-09 21:45:01 -0300

    adicionando mais filtros

--- filters.py
+++ filters.py
@@ -45,8 +45,14 @@ imgBilateral5x5 = cv.bilateralFilter(imgGrayscale, 9, 75, 75)
 # Bilateral 15x15
 imgBilateral15x15 = cv.bilateralFilter(imgGrayscale, 9, 75, 75)
 
-# Canny
-imgCanny = cv.Canny(imgGrayscale, 100, 200)
+# Canny 1
+imgCanny1 = cv.Canny(imgGrayscale, 100, 200)
+
+# Canny 2
+imgCanny2 = cv.Canny(imgBlurred5x5, 100, 200)
+
+# Canny 3
+imgCanny3 = cv.Canny(imgBlurred15x15, 100, 200)
 
 # Sobel
 imgSobel = cv.Sobel(imgGrayscale, cv.CV_64F, 1, 0, ksize=5)
@@ -66,6 +72,21 @@ imgErosion = cv.erode(imgGrayscale,kernel,iterations = 1)
 # Dilation
 imgDilation = cv.dilate(imgGrayscale,kernel,iterations = 1)
 
+# Opening
+imgOpening = cv.morphologyEx(imgGrayscale, cv.MORPH_OPEN, kernel)
+
+# Closing
+imgClosing = cv.morphologyEx(imgGrayscale, cv.MORPH_CLOSE, kernel)
+
+# Morphological Gradient
+imgMorphGradient = cv.morphologyEx(imgGrayscale, cv.MORPH_GRADIENT, kernel)
+
+# Top Hat
+imgTopHat = cv.morphologyEx(imgGrayscale, cv.MORPH_TOPHAT, kernel)
+
+# Black Hat
+imgBlackHat = cv.morphologyEx(imgGrayscale, cv.MORPH_BLACKHAT, kernel)
+
 # cv.imshow('Original', img)
 # cv.imshow('Original com 0', img0)
 # cv.imshow('Grayscale', imgGrayscale)
@@ -80,12 +101,19 @@ imgDilation = cv.dilate(imgGrayscale,kernel,iterations = 1)
 # cv.imshow('Median 15x15', imgMedian15x15)
 # cv.imshow('Bilateral 5x5', imgBilateral5x5)
 # cv.imshow('Bilateral 15x15', imgBilateral15x15)
-# cv.imshow('Canny', imgCanny)
+# cv.imshow('Canny 1', imgCanny1)
+# cv.imshow('Canny 2', imgCanny2)
+# cv.imshow('Canny 3', imgCanny3)
 # cv.imshow('Sobel', imgSobel)
 # cv.imshow('Laplacian', imgLaplacian)
 # cv.imshow('Laplacian 2', imgLaplacian2)
 # cv.imshow('Laplacian 3', imgLaplacian3)
-cv.imshow('Erosion', imgErosion)
-cv.imshow('Dilation', imgDilation)
+# cv.imshow('Erosion', imgErosion)
+# cv.imshow('Dilation', imgDilation)
+# cv.imshow('Opening', imgOpening)
+# cv.imshow('Closing', imgClosing)
+# cv.imshow('Morphological Gradient', imgMorphGradient)
+# cv.imshow('Top Hat', imgTopHat)
+cv.imshow('Black Hat', imgBlackHat)
 
 k = cv.waitKey(0)
